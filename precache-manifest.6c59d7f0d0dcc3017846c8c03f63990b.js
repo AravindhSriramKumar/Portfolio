f76846e467a3721e8a756a2358b467e947e5712f
aravindh <aravindhsriram310@gmail.com>
2025-01-03 12:00:49 +0530

    Updates

---
+++ precache-manifest.6c59d7f0d0dcc3017846c8c03f63990b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c293c50c4189bcc8181bdc91334519",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d4579ee79241ec99c270",
+    "url": "/Portfolio/static/css/main.1eddfd3f.chunk.css"
+  },
+  {
+    "revision": "d8cd1ed26c5d318adadb",
+    "url": "/Portfolio/static/js/2.a93d0bea.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/Portfolio/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "d4579ee79241ec99c270",
+    "url": "/Portfolio/static/js/main.87b5c3ac.chunk.js"
+  },
+  {
+    "revision": "2e59aa2d5071e00d8a98",
+    "url": "/Portfolio/static/js/runtime~main.1afa9ca6.js"
+  },
+  {
+    "revision": "eaf7a44ff4681c0fad56b1be9155544c",
+    "url": "/Portfolio/static/media/Resume_AravindhSriramKumar_A_G.eaf7a44f.pdf"
+  },
+  {
+    "revision": "2a38371708e817015a291f8736bec68e",
+    "url": "/Portfolio/static/media/profile.2a383717.jpg"
+  }
+]);
