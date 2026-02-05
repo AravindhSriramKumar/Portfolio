7f10c4da7fe22790f7fea43bd32ec97c1265424d
Aravindh sriram kumar A G <aravindhsriram310@gmail.com>
2026-02-05 11:10:54 +0530

    fix: add footer text and hyperlink for SeNSE Lab, IIT M

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>SeNSE Lab, IIT M</p>
+      <a href='https://sense.cse.iitm.ac.in/' target='_blank' rel='noopener noreferrer'>Visit SeNSE Lab</a>
+    </footer>
+  );
+};
+
+export default Footer;
